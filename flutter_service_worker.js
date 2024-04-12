f17d13eda60ec35e5b16f30da9bcbbbecdd4fa59
nextdaysoft <88833804+nextdaysoft@users.noreply.github.com>
2024-04-12 14:19:11 +0530

    fixing web 13 build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,11 +4,11 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "7d7dedecfb601c840a70dafacaaba320",
-"index.html": "2bf317ccd823f94bf808d959d3e73aaa",
-"/": "2bf317ccd823f94bf808d959d3e73aaa",
-"main.dart.js": "c4d5ee51666c7f95c10271e7a8896b5a",
+"index.html": "535d02f7f7e8f5cd309b0cdd3c493b8b",
+"/": "535d02f7f7e8f5cd309b0cdd3c493b8b",
+"main.dart.js": "dfb07b7911c6bea20dde6754eb9ee07d",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "9dacb56cf3e7df4381307151ae4a4f02",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
